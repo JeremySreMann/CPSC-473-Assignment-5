452d65ca24ca2432a11e9ac7843a7e0980b58d54
JeremySreMann <jersremann@gmail.com>
2017-03-06 21:10:58 -0800

    Update tests.js

--- coffeerun/scripts/tests.js
+++ coffeerun/scripts/tests.js
@@ -1,3 +1,19 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
+QUnit.test('DataStore', function(assert) {
+    var ds = new App.DataStore();
+  
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
+  
+    assert.equal(ds.getAll(), {
+      'james@bond.com': 'eshpressho',
+      'm@bond.com': 'tea'
+    });
+    ds.remove('james@bond.com');
+
+    assert.equal(ds.getAll(), {
+        'm@bond.com': 'tea'
+    });
+
+    assert.equal(ds.get('m@bond.com'), 'tea');
+    assert.equal(ds.get('james@bond.com'), undefined);
+  });
